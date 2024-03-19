b64b9d0e3f7ea288704bdc62a72e817d87808431
Mitch Banks <reynoch94@live.com>
2024-03-19 13:33:10 +1300

    Setting up folder structure

---
+++ src/pages/ProjectSubmissions/ProjectSubmissions.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ProjectSubmissions() {
+  return <div>ProjectSubmissions</div>;
+}
+
+export default ProjectSubmissions;
---
+++ src/pages/TeacherProfileViewer/TeacherProfileViewer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TeacherProfileViewer() {
+  return <div>TeacherProfileViewer</div>;
+}
+
+export default TeacherProfileViewer;
