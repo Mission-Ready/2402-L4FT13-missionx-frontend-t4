ebbfe917e6b550c8499b94cc9b8d9cd5831ff20a
Mitch Banks <reynoch94@live.com>
2024-03-21 21:58:37 +1300

    Set up links in side bar and styled a bit

--- src/pages/CommonComponents/SideBar.jsx
+++ src/pages/CommonComponents/SideBar.jsx
@@ -1,5 +1,6 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import styles from "./SideBar.module.css";
+import { NavLink } from "react-router-dom";
 
 function SideBar() {
   const [isActive, setIsActive] = useState(false);
@@ -7,17 +8,30 @@ function SideBar() {
   const toggleSideBar = () => {
     setIsActive(!isActive);
   };
+
   return (
     <>
-      <div className={`${styles.sidebar} ${isActive ? styles.isActive : ""}`}>
-        <div>
-          <ul>
-            <li>PROGRESS TRACKER</li>
-            <li>STUDENT PROFILES</li>
-            <li>HELP REQUESTS</li>
-            <li>PROJECT SUBMISSIONS</li>
-            <li>PROJECT LIBRARY</li>
-          </ul>
+      <div
+        className={`${styles.teacherSideBar} ${
+          isActive ? styles.isActive : ""
+        }`}
+      >
+        <div className={styles.teacherSideBarList}>
+          <NavLink to="/progress-tracker" className={styles.sidebarLink}>
+            PROGRESS TRACKER
+          </NavLink>
+          <NavLink to="/student-profiles" className={styles.sidebarLink}>
+            STUDENT PROFILES
+          </NavLink>
+          <NavLink to="/help-requests" className={styles.sidebarLink}>
+            HELP REQUESTS
+          </NavLink>
+          <NavLink to="/project-submissions" className={styles.sidebarLink}>
+            PROJECT SUBMISSIONS
+          </NavLink>
+          <NavLink to="/project-library" className={styles.sidebarLink}>
+            PROJECT LIBRARY
+          </NavLink>
         </div>
         <button onClick={toggleSideBar}>toggle</button>
       </div>
